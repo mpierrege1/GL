7947dd47f4b7f12308c6c8fb45dda8a32cda8411
mpierrege1 <44799284+mpierrege1@users.noreply.github.com>
2018-11-13 13:50:57 +0100

    Exo 44 done

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
+#include "TP_lib.h"
+
 
 int main(void)
 {
+	short resultat, i = 0;
+	// 10 Lancer de dé
+	while (i<10)
+	{
+	resultat = LancerDe();
+	i++;
+	// 	On Garde uniquement la valeur du dernier
+	// affichage de la valeur du dé
+	}
+		printf("Le de montre un %hd !", resultat);   
     return 0;
 }
