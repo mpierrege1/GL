dd292bf0c1be2931ad88f4ac2edfead8299cf7de
mpierrege1 <44799284+mpierrege1@users.noreply.github.com>
2018-11-14 23:55:30 +0100

    Exo 45 done

--- src/main.c
+++ src/main.c
@@ -2,8 +2,31 @@
 //
 
 #include "my_header.h"
+#include "cvi_gui.h"
 
 int main(void)
 {
+	char over = 0;
+	long score = 0;
+	long de = 0;
+			 
+  	while (de != 1)
+	{
+		de = GetLong("Rentrez le numero du de");  
+		if ( de == 2 || de == 4 || de == 6 )
+		{
+			score = score + de;	
+		}
+		if ( de == 3)
+		{
+			score = score * 2;	
+		}
+		if ( de == 5 && score >= 2)
+		{
+			score = score - 2;	
+		}
+		
+	}		 
+	DisplayLong ("Score Total" ,score);
     return 0;
 }
