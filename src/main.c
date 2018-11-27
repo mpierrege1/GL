cc8142af830be5aa1c811e342c290f38bc34e4f6
maxime pierre <maxime.pierre@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:48:24 +0100

    Bibliothéque ajouté

--- src/main.c
+++ src/main.c
@@ -2,8 +2,38 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h" 
 
 int main(void)
 {
-    return 0;
+     unsigned char tirage[6];
+	 unsigned char user[6];
+	 unsigned char cpttirage, cptuser, nmbcommun = 0, tour = 0, highscore = 0;
+	 // Variable nmbcommun ne sert a compter le nombre de nombre commun entre le tirage et la grille de l'utilisateur
+	 // Tirage alléatoire des nombre choisi par l'utilisateur
+	 initialiserTirage()  ;
+	 for(cptuser=0; cptuser<6; cptuser++)
+	 {
+		user[cptuser] = tirerNumero();	 
+	 }
+		// Tirage alléatoire du tirage
+	 	initialiserTirage();
+	 	for(cpttirage=0; cpttirage<6; cpttirage++)
+	 	{
+			tirage[cpttirage] = tirerNumero();	 
+		}
+	 	//   Comparaison des nombres commun entre l'utilisateur et le tirage
+	 
+	 	for(cptuser=0; cptuser<6; cptuser++)
+	 	{
+	   		for (cpttirage=0; cpttirage<6; cpttirage++) // On doit test le premier numéro d'user avec l'ensemble de tirage   
+	  		{	   
+	   			if (user[cptuser] == tirage[cpttirage])
+				{
+			  		nmbcommun++;
+				}	
+	   		}				
+	
+	 	}
+	return 0;
 }
