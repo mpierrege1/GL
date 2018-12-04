8e85d9a6f34b4fb0d59334a1d82b47b270b1bea4
maxime pierre <maxime.pierre@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:42:26 +0100

    Exo79 done

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <userint.h>
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
-    return 0;
+	char nom[100];
+	char prenom[100];
+	long age = 0;
+	char phrase[100];
+	printf("Inserer votre nom SVP :");
+	scanf("%s", nom);
+	printf("Inserer votre prenom SVP :"); 
+	scanf("%s", prenom);
+	printf("Insérer votre age SVP :"); 
+	scanf("%d", &age);
+	sprintf(phrase,"je m'appelle %s %s et j'ai %d ans", prenom, nom, age);
+	MessagePopup("Bonjour et bienvenue", phrase);
+	return 0;
 }
